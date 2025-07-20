020b2949b27196971c882ff92d072d9b5b722054
1Kuso4ek1 <dimasalov2006@gmail.com>
2025-07-20 18:03:55 +0300

    Basic initial setup, REST api specification, postgres setup, table setup, model setup

---
+++ sql/create_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE pastes (
+    id TEXT PRIMARY KEY,
+    text TEXT NOT NULL,
+    language TEXT DEFAULT 'plaintext',
+    created TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    expires TIMESTAMPTZ,
+    viewCount INTEGER NOT NULL DEFAULT 0,
+    ipAddress INET
+);
+
+CREATE INDEX idx_expiration ON pastes (expires);
